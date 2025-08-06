62e204a378ccf7d09f7ff158800eaf7693841cfa
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-06 14:57:25 +0200

    feat: using that Themed Button to have an easy btn that currently prints pressed

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,16 +1,24 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 import ThemedView from "../../components/ThemedView";
 import Spacer from "../../components/Spacer";
 import ThemedText from "../../components/ThemedText";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/Colors";
+import ThemedButton from "../../components/ThemedButton";
 
 const Login = () => {
+  const handleSubmit = () => {
+    console.log("pressed");
+  };
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Login to your account.
       </ThemedText>
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}> Login </Text>
+      </ThemedButton>
       <Spacer height={100} />
       <Link href="/register">
         <ThemedText style={{ textAlign: "center" }}>
@@ -27,9 +35,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,16 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import ThemedView from "../../components/ThemedView";
 import Spacer from "../../components/Spacer";
 import ThemedText from "../../components/ThemedText";
 import { Link } from "expo-router";
+import ThemedButton from "../../components/ThemedButton";
 
 const Register = () => {
+  const handleSubmit = () => {
+    console.log("pressed");
+  };
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Register a New account.
       </ThemedText>
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Register</Text>
+      </ThemedButton>
       <Spacer height={100} />
       <Link href="/login">
         <ThemedText style={{ textAlign: "center" }}>Login Instead</ThemedText>
@@ -25,6 +32,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
