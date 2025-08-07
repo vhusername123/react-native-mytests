614ca945ecbfeafb560f321100e2acf73ec6d96e
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-07 10:24:17 +0200

    feat: adds login and register functionallity

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,11 +1,27 @@
 import { createContext, useState } from "react";
-
+import { account } from "../lib/appwrite";
+import { ID } from "react-native-appwrite";
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
-  async function login(email, password) {}
-  async function register(email, password) {}
+  async function login(email, password) {
+    try {
+      await account.createEmailPasswordSession(email, password);
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function register(email, password) {
+    try {
+      await account.create(ID.unique(), email, password);
+      await login(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   async function logout() {}
   return (
     <UserContext.Provider value={{ user, login, register, logout }}>
