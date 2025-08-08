3723a4f719de6cc638d5672302ce88d790b06cf0
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-08 14:43:53 +0200

    feat: invoking the logout on the profile page via a button

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,9 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import { useUser } from "../../hooks/useUser";
+import ThemedButton from "../../components/ThemedButton";
+import { useState } from "react";
+import { Colors } from "../../constants/Colors";
 
 const Profile = () => {
+  const [error, setError] = useState("");
+  const { logout } = useUser();
+  async function logoutattempt() {
+    setError(null);
+    try {
+      await logout();
+    } catch (error) {
+      setError(error.message);
+    }
+  }
   return (
     <ThemedView style={styles.container}>
       <ThemedText title={true} style={styles.heading}>
@@ -12,6 +26,11 @@ const Profile = () => {
       <Spacer />
       <ThemedText>Time to start reading some books...</ThemedText>
       <Spacer />
+      <ThemedButton onPress={logoutattempt}>
+        <Text style={{ color: "#f2f2f2" }}>logout</Text>
+      </ThemedButton>
+      <Spacer />
+      {(error && <Text style={styles.error}>{error}</Text>) || <Spacer />}
     </ThemedView>
   );
 };
@@ -28,5 +47,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
     textAlign: "center",
+  },
+  error: {
+    color: Colors.warning,
+    padding: 10,
+    backgroundColor: "#f5c1c8",
+    borderColor: Colors.warning,
+    borderWidth: 1,
+    borderRadius: 6,
+    marginHorizontal: 10,
   },
 });
