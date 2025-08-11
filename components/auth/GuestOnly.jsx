33d6d602c78ed594e8040bdf10da0def348ea55c
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-11 11:24:37 +0200

    feat: GuestOnly to protect pages that only available when loggedout

---
+++ components/auth/GuestOnly.jsx
@@ -0,0 +1,19 @@
+import { useRouter } from "expo-router";
+import { useUser } from "../../hooks/useUser";
+import { useEffect } from "react";
+import { Text } from "react-native";
+
+const GuestOnly = ({ children }) => {
+  const { user, authChecked } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (user) {
+      router.replace("/profile");
+    }
+  }, [user, authChecked]);
+  if (!authChecked) {
+    return <Text>Loading</Text>;
+  }
+  return children;
+};
+export default GuestOnly;
