5f3acad8aed311570c1f4aa36915894d438da22e
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-11 12:12:45 +0200

    feat: wrapping the entire app in the BooksContext

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,33 +3,35 @@ import { Stack } from "expo-router";
 import { Colors } from "../constants/Colors";
 import { StatusBar } from "expo-status-bar";
 import { UserProvider } from "../context/UserContext";
+import { BooksProvider } from "../context/BooksContext";
 const RootLayout = () => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: theme.navBackground,
-          },
-          headerTintColor: theme.title,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "Home",
-            headerShown: true,
-            headerBackVisible: false,
-          }}
-        />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen name="contact" options={{ title: "Contact" }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-      </Stack>
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.navBackground,
+            },
+            headerTintColor: theme.title,
+          }}>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Home",
+              headerShown: true,
+              headerBackVisible: false,
+            }}
+          />
+          <Stack.Screen name="about" options={{ title: "About" }} />
+          <Stack.Screen name="contact" options={{ title: "Contact" }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 };
