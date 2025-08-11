c2eb5b4e835b85eddbead9aefd90254359e9070a
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-11 11:24:50 +0200

    feat: implementing GuestOnly

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
+import GuestOnly from "../../components/auth/GuestOnly";
 
 const AuthLayout = () => {
   return (
-    <>
+    <GuestOnly>
       <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false, animation: "none" }} />
-    </>
+    </GuestOnly>
   );
 };
 
