b52b4010044b2ba5d2025da7e59c7cbcb708bd7f
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-12 14:25:43 +0200

    feat: combining all languages into a list

--- context/LanguageContext.js
+++ context/LanguageContext.js
@@ -1,9 +1,11 @@
 import { createContext, useState } from "react";
+import { Language } from "../constants/Language";
 export const LanguageContext = createContext();
 export function LanguageProvider({ children }) {
   const [userLang, setLanguage] = useState("en");
+  const langList = Object.keys(Language);
   return (
-    <LanguageContext.Provider value={{ userLang, setLanguage }}>
+    <LanguageContext.Provider value={{ userLang, setLanguage, langList }}>
       {children}
     </LanguageContext.Provider>
   );
